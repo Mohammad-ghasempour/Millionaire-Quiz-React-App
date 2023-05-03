bb6dd3579924fb501d0113b5aae9703451cbe84b
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-05-03 15:14:49 +0200

    css container and two side of overall view created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main">Main</div>
+      <div className="pyramid">Pyramid</div>
+     
+    </div>
+  );
+}
+
+export default App;
