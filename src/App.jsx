3a7896540c6f15ff5538f68266ddddc11696d113
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-05-03 17:30:01 +0200

    pyramid of amount created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import "./app.css";
 
 function App() {
-  return (
-    <div className="app">
-      <div className="main">Main</div>
-      <div className="pyramid">Pyramid</div>
-     
-    </div>
-  );
+   const moneyPyramit = [
+      { id: 1, amount: "$ 100" },
+      { id: 2, amount: "$ 200" },
+      { id: 3, amount: "$ 300" },
+      { id: 4, amount: "$ 500" },
+      { id: 5, amount: "$ 1000" },
+      { id: 6, amount: "$ 2000" },
+      { id: 7, amount: "$ 4000" },
+      { id: 8, amount: "$ 8000" },
+      { id: 9, amount: "$ 16000" },
+      { id: 10, amount: "$ 32000" },
+      { id: 11, amount: "$ 64000" },
+      { id: 12, amount: "$ 125000" },
+      { id: 13, amount: "$ 250000" },
+      { id: 14, amount: "$ 500000" },
+      { id: 15, amount: "$ 1000000" },
+   ].reverse();
+   return (
+      <div className="app">
+         <div className="main">Main</div>
+         <div className="pyramid">
+            <ul className="moneyList">
+               {moneyPyramit.map((item) => (
+                  <li className="moneyListItem">
+                     <span className="moneyListItemNumber">{item.id}</span>
+                     <span className="moneyListItemAmount">{item.amount}</span>
+                  </li>
+               ))}
+            </ul>
+         </div>
+      </div>
+   );
 }
 
 export default App;
