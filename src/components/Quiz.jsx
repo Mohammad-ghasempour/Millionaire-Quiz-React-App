c8020a57bfa7e2f0df147b58bd986160e6d63d33
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-05-19 14:32:43 +0200

    sounds for starting the application, correct answer and wrong answer have been set

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,9 @@
 import { useState, useEffect } from "react";
+import useSound from "use-sound";
+import play from "../assets/src_sounds_play.mp3";
+import correct from "../assets/src_sounds_correct.mp3";
+import wrong from "../assets/src_sounds_wrong.mp3";
+
 const Quiz = ({
    data,
    setStop,
@@ -9,7 +14,13 @@ const Quiz = ({
    const [question, setQuestion] = useState(null);
    const [selectedAnswer, setSelectedAnswer] = useState(null);
    const [className, setClassName] = useState("answer");
+   const [letsPlay] = useSound(play);
+   const [correctAnswer] = useSound(correct);
+   const [wrongAnswer] = useSound(wrong);
 
+   useEffect(() => {
+      letsPlay();
+   }, [letsPlay]);
 
    useEffect(() => {
       setQuestion(data[questionNumber - 1]);
@@ -27,17 +38,24 @@ const Quiz = ({
       }, duration);
    };
    const handleClick = (answer) => {
-     // setPause(true);
+      // setPause(true);
       setSelectedAnswer(answer);
       setClassName("answer active");
       delay(3000, () => {
          setClassName(answer.correct ? "answer correct" : "answer wrong");
       });
-      delay(7000, () => {
+      delay(5000, () => {
          if (answer.correct) {
-            setQuestionNumber((prev) => prev + 1);
+            correctAnswer();
+            delay(8000,()=>{
+               setQuestionNumber((prev) => prev + 1);
+               setSelectedAnswer(null);
+            })
          } else {
-            setStop(true);
+            wrongAnswer();
+            delay(5000,()=>{
+               setStop(true);
+            })
          }
       });
    };
